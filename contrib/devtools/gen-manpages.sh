7463c0c280e2f6a1316e926f4dfb6f50e46d1709
Netesy [Maximus] Emmanuel <netesy@users.noreply.github.com>
2021-02-09 13:03:32 +0100

    Update

--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -7,11 +7,11 @@ BUILDDIR=${BUILDDIR:-$TOPDIR}
 BINDIR=${BINDIR:-$BUILDDIR/src}
 MANDIR=${MANDIR:-$TOPDIR/doc/man}
 
-BITCOIND=${BITCOIND:-$BINDIR/litecoind}
-BITCOINCLI=${BITCOINCLI:-$BINDIR/litecoin-cli}
-BITCOINTX=${BITCOINTX:-$BINDIR/litecoin-tx}
-WALLET_TOOL=${WALLET_TOOL:-$BINDIR/litecoin-wallet}
-BITCOINQT=${BITCOINQT:-$BINDIR/qt/litecoin-qt}
+BITCOIND=${BITCOIND:-$BINDIR/africoind}
+BITCOINCLI=${BITCOINCLI:-$BINDIR/africoin-cli}
+BITCOINTX=${BITCOINTX:-$BINDIR/africoin-tx}
+WALLET_TOOL=${WALLET_TOOL:-$BINDIR/africoin-wallet}
+BITCOINQT=${BITCOINQT:-$BINDIR/qt/africoin-qt}
 
 [ ! -x $BITCOIND ] && echo "$BITCOIND not found or not executable." && exit 1
 
--- contrib/testgen/gen_key_io_test_vectors.py
+++ contrib/testgen/gen_key_io_test_vectors.py
@@ -71,28 +71,28 @@ templates = [
 # templates for valid bech32 sequences
 bech32_templates = [
   # hrp, version, witprog_size, metadata, output_prefix
-  ('ltc',   0, 20, (False, 'main',    None, True), p2wpkh_prefix),
-  ('ltc',   0, 32, (False, 'main',    None, True), p2wsh_prefix),
-  ('ltc',   1,  2, (False, 'main',    None, True), (OP_1, 2)),
-  ('tltc',  0, 20, (False, 'test',    None, True), p2wpkh_prefix),
-  ('tltc',  0, 32, (False, 'test',    None, True), p2wsh_prefix),
-  ('tltc',  2, 16, (False, 'test',    None, True), (OP_2, 16)),
-  ('rltc',  0, 20, (False, 'regtest', None, True), p2wpkh_prefix),
-  ('rltc',  0, 32, (False, 'regtest', None, True), p2wsh_prefix),
-  ('rltc', 16, 40, (False, 'regtest', None, True), (OP_16, 40))
+  ('afc',   0, 20, (False, 'main',    None, True), p2wpkh_prefix),
+  ('afc',   0, 32, (False, 'main',    None, True), p2wsh_prefix),
+  ('afc',   1,  2, (False, 'main',    None, True), (OP_1, 2)),
+  ('tafc',  0, 20, (False, 'test',    None, True), p2wpkh_prefix),
+  ('tafc',  0, 32, (False, 'test',    None, True), p2wsh_prefix),
+  ('tafc',  2, 16, (False, 'test',    None, True), (OP_2, 16)),
+  ('rafc',  0, 20, (False, 'regtest', None, True), p2wpkh_prefix),
+  ('rafc',  0, 32, (False, 'regtest', None, True), p2wsh_prefix),
+  ('rafc', 16, 40, (False, 'regtest', None, True), (OP_16, 40))
 ]
 # templates for invalid bech32 sequences
 bech32_ng_templates = [
   # hrp, version, witprog_size, invalid_bech32, invalid_checksum, invalid_char
   ('tc',    0, 20, False, False, False),
-  ('tltc',  17, 32, False, False, False),
-  ('rltc',  3,  1, False, False, False),
-  ('ltc',   15, 41, False, False, False),
-  ('tltc',  0, 16, False, False, False),
-  ('rltc',  0, 32, True,  False, False),
-  ('ltc',   0, 16, True,  False, False),
-  ('tltc',  0, 32, False, True,  False),
-  ('rltc',  0, 20, False, False, True)
+  ('tafc',  17, 32, False, False, False),
+  ('rafc',  3,  1, False, False, False),
+  ('afc',   15, 41, False, False, False),
+  ('tafc',  0, 16, False, False, False),
+  ('rafc',  0, 32, True,  False, False),
+  ('afc',   0, 16, True,  False, False),
+  ('tafc',  0, 32, False, True,  False),
+  ('rafc',  0, 20, False, False, True)
 ]
 
 def is_valid(v):
@@ -112,7 +112,7 @@ def is_valid(v):
 
 def is_valid_bech32(v):
     '''Check vector v for bech32 validity'''
-    for hrp in ['ltc', 'tltc', 'rltc']:
+    for hrp in ['afc', 'tafc', 'rafc']:
         if decode(hrp, v) != (None, None):
             return True
     return False
